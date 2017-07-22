6d3b4ba775dab6dbf440c98a2c609a0bb5bd797e
AtumVirt <atum@atumvirt.com>
2017-07-22 08:46:15 -0700

    dock and autosize / minimum size for propertygrids

--- PVS DiskUP/MainForm.Designer.cs
+++ PVS DiskUP/MainForm.Designer.cs
@@ -28,80 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
-            this.propertyGrid2 = new System.Windows.Forms.PropertyGrid();
-            this.btnLoad = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // propertyGrid1
-            // 
-            this.propertyGrid1.CategoryForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.propertyGrid1.Location = new System.Drawing.Point(2, 2);
-            this.propertyGrid1.Margin = new System.Windows.Forms.Padding(2);
-            this.propertyGrid1.MinimumSize = new System.Drawing.Size(354, 532);
-            this.propertyGrid1.Name = "propertyGrid1";
-            this.propertyGrid1.Size = new System.Drawing.Size(354, 532);
-            this.propertyGrid1.TabIndex = 1;
-            // 
-            // propertyGrid2
-            // 
-            this.propertyGrid2.CategoryForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.propertyGrid2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.propertyGrid2.Location = new System.Drawing.Point(360, 2);
-            this.propertyGrid2.Margin = new System.Windows.Forms.Padding(2);
-            this.propertyGrid2.MinimumSize = new System.Drawing.Size(354, 532);
-            this.propertyGrid2.Name = "propertyGrid2";
-            this.propertyGrid2.Size = new System.Drawing.Size(362, 532);
-            this.propertyGrid2.TabIndex = 2;
-            // 
-            // btnLoad
-            // 
-            this.btnLoad.Location = new System.Drawing.Point(7, 6);
-            this.btnLoad.Margin = new System.Windows.Forms.Padding(2);
-            this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(61, 28);
-            this.btnLoad.TabIndex = 0;
-            this.btnLoad.Text = "Load";
-            this.btnLoad.UseVisualStyleBackColor = true;
-            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.AutoSize = true;
-            this.flowLayoutPanel1.Controls.Add(this.propertyGrid1);
-            this.flowLayoutPanel1.Controls.Add(this.propertyGrid2);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(74, 6);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(725, 536);
-            this.flowLayoutPanel1.TabIndex = 3;
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(811, 540);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.btnLoad);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "MainForm";
-            this.Text = "PVS DiskUP";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+      this.propertyGrid2 = new System.Windows.Forms.PropertyGrid();
+      this.menuStrip1.SuspendLayout();
+      this.panel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(1003, 24);
+      this.menuStrip1.TabIndex = 3;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+      this.fileToolStripMenuItem.Text = "File";
+      // 
+      // openToolStripMenuItem
+      // 
+      this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+      this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+      this.openToolStripMenuItem.Text = "Open";
+      this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+      // 
+      // toolStripSeparator1
+      // 
+      this.toolStripSeparator1.Name = "toolStripSeparator1";
+      this.toolStripSeparator1.Size = new System.Drawing.Size(100, 6);
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+      this.exitToolStripMenuItem.Text = "Exit";
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.panel1.Controls.Add(this.propertyGrid1);
+      this.panel1.Controls.Add(this.propertyGrid2);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel1.Location = new System.Drawing.Point(0, 24);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(1003, 637);
+      this.panel1.TabIndex = 5;
+      // 
+      // propertyGrid1
+      // 
+      this.propertyGrid1.CategoryForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+      this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Left;
+      this.propertyGrid1.LineColor = System.Drawing.SystemColors.ControlDark;
+      this.propertyGrid1.Location = new System.Drawing.Point(500, 0);
+      this.propertyGrid1.Margin = new System.Windows.Forms.Padding(2);
+      this.propertyGrid1.MinimumSize = new System.Drawing.Size(354, 532);
+      this.propertyGrid1.Name = "propertyGrid1";
+      this.propertyGrid1.Size = new System.Drawing.Size(500, 637);
+      this.propertyGrid1.TabIndex = 5;
+      // 
+      // propertyGrid2
+      // 
+      this.propertyGrid2.CategoryForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+      this.propertyGrid2.Dock = System.Windows.Forms.DockStyle.Left;
+      this.propertyGrid2.LineColor = System.Drawing.SystemColors.ControlDark;
+      this.propertyGrid2.Location = new System.Drawing.Point(0, 0);
+      this.propertyGrid2.Margin = new System.Windows.Forms.Padding(2);
+      this.propertyGrid2.MinimumSize = new System.Drawing.Size(354, 532);
+      this.propertyGrid2.Name = "propertyGrid2";
+      this.propertyGrid2.Size = new System.Drawing.Size(500, 637);
+      this.propertyGrid2.TabIndex = 4;
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.ClientSize = new System.Drawing.Size(1003, 661);
+      this.Controls.Add(this.panel1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Margin = new System.Windows.Forms.Padding(2);
+      this.MinimumSize = new System.Drawing.Size(732, 600);
+      this.Name = "MainForm";
+      this.Text = "PVS DiskUP";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.panel1.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PropertyGrid propertyGrid1;
-        private System.Windows.Forms.PropertyGrid propertyGrid2;
-        private System.Windows.Forms.Button btnLoad;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-    }
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.PropertyGrid propertyGrid1;
+    private System.Windows.Forms.PropertyGrid propertyGrid2;
+  }
 }
 
--- PVS DiskUP/MainForm.cs
+++ PVS DiskUP/MainForm.cs
@@ -19,33 +19,33 @@ namespace PVS_DiskUP
             InitializeComponent();
         }
 
-        private void btnLoad_Click(object sender, EventArgs e)
-        {
-            var ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                var stream = ofd.OpenFile();
-                var header = stream.ReadStructure<VDISK_HEADER>();
-                var footer = stream.ReadStructure<VDISK_FOOTER>();
-                if (header == null || footer == null)
-                {
-                    MessageBox.Show(Resources.ReadPVPFailureText);
-                }
-                else
-                {
-                    propertyGrid1.SelectedObject = new VDISK_HEADER_WRAPPER(header.Value);
-                    propertyGrid1.Text = Resources.HeaderTitle;
-
-                    propertyGrid2.SelectedObject = new VDISK_FOOTER_WRAPPER(footer.Value);
-                    propertyGrid2.Text = Resources.FooterTitle;
-                }
-            }
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
+    private void openToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      var ofd = new OpenFileDialog();
+      if (ofd.ShowDialog() == DialogResult.OK)
+      {
+        var stream = ofd.OpenFile();
+        var header = stream.ReadStructure<VDISK_HEADER>();
+        var footer = stream.ReadStructure<VDISK_FOOTER>();
+        if (header == null || footer == null)
+        {
+          MessageBox.Show(Resources.ReadPVPFailureText);
+        }
+        else
+        {
+          propertyGrid1.SelectedObject = new VDISK_HEADER_WRAPPER(header.Value);
+          propertyGrid1.Text = Resources.HeaderTitle;
+
+          propertyGrid2.SelectedObject = new VDISK_FOOTER_WRAPPER(footer.Value);
+          propertyGrid2.Text = Resources.FooterTitle;
+        }
+      }
     }
+  }
 }
